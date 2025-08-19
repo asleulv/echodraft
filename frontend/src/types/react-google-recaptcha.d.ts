dd2fb22cef2b3d1791f711e3fdca6b5293a035de
asleulv <101048079+asleulv@users.noreply.github.com>
2025-08-19 14:14:04 +0200

    Adjusted recaptcha file

--- frontend/src/types/react-google-recaptcha.d.ts
+++ frontend/src/types/react-google-recaptcha.d.ts
@@ -1,1 +1,16 @@
-declare module 'react-google-recaptcha';
+declare module 'react-google-recaptcha' {
+  import { Component } from 'react';
+
+  interface ReCAPTCHAProps {
+    sitekey: string;
+    onChange?: (token: string | null) => void;
+    theme?: 'light' | 'dark';
+    size?: 'compact' | 'normal' | 'invisible';
+    tabIndex?: number;
+  }
+
+  export default class ReCAPTCHA extends Component<ReCAPTCHAProps> {
+    reset(): void;
+    execute(): void;
+  }
+}
